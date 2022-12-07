8b7e8561abe170e3f2d0b79907b64686981ad7ad
manju-sri <107233576+manju-sri@users.noreply.github.com>
2022-12-08 02:05:25 +0530

    Delete module-info.java

--- module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.supplychainmanagement {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.supplychainmanagement to javafx.fxml;
-    exports com.example.supplychainmanagement;
-}
